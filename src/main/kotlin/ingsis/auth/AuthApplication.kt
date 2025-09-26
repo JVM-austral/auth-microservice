eb7669156af46212c39e43b1094c483af38e44ea
VitoContestabile <vcontestabilemonti@mail.austral.edu ar>
2025-09-26 15:06:04 -0300

    ci and config added

--- src/main/kotlin/ingsis/auth/AuthApplication.kt
+++ src/main/kotlin/ingsis/auth/AuthApplication.kt
@@ -1,4 +1,4 @@
-package ingsis.Auth
+package ingsis.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class AuthApplication
 
 fun main(args: Array<String>) {
-	runApplication<AuthApplication>(*args)
+    runApplication<AuthApplication>(*args)
 }
--- src/test/kotlin/ingsis/auth/AuthApplicationTests.kt
+++ src/test/kotlin/ingsis/auth/AuthApplicationTests.kt
@@ -1,13 +1,11 @@
-package ingsis.Auth
+package ingsis.auth
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AuthApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
