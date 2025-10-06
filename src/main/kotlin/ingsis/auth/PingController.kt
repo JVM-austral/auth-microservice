86401b2c3ecbd52ed9ba447bc6162671a886fb5a
juanitorres1 <jitorres@mail.austral.edu.ar>
2025-10-06 15:54:20 -0300

    testing the service connection

---
+++ src/main/kotlin/ingsis/auth/PingController.kt
@@ -0,0 +1,13 @@
+package ingsis.auth
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
+}
