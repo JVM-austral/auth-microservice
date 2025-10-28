5bc0740b0b850327bfa24834d442464171785c69
juanitorres1 <jitorres@mail.austral.edu.ar>
2025-10-27 23:30:53 -0300

    solving ktlint conflicts

--- src/main/kotlin/ingsis/auth/controller/SnippetPermissionValidateController.kt
+++ src/main/kotlin/ingsis/auth/controller/SnippetPermissionValidateController.kt
@@ -5,6 +5,4 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/snippet-permissions")
-class SnippetPermissionValidateController(
-) {
-}
+class SnippetPermissionValidateController
